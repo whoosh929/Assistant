b6688b62d30e67e08aff1b4a246310b4647aa681
Kevin Shyu <kevin.shyu@gmail.com>
2024-03-30 16:31:03 -0700

    fixed indentation issue in builder_config.py

--- core/builder_config.py
+++ core/builder_config.py
@@ -9,12 +9,12 @@ import os
 from llama_index.llms import OpenAI
 
 # set OpenAI Key - use Streamlit secrets
-os.environ["OPENAI_API_KEY"] = st.secrets.openai_key
+#os.environ["OPENAI_API_KEY"] = st.secrets.openai_key
 # load LLM
-BUILDER_LLM = OpenAI(model="gpt-4-1106-preview")
+#BUILDER_LLM = OpenAI(model="gpt-4-1106-preview")
 
 # # Anthropic (make sure you `pip install anthropic`)
-# from llama_index.llms import Anthropic
-# # set Anthropic key
-# os.environ["ANTHROPIC_API_KEY"] = st.secrets.anthropic_key
-# BUILDER_LLM = Anthropic()
+from llama_index.llms import Anthropic
+ # set Anthropic key
+os.environ["ANTHROPIC_API_KEY"] = st.secrets.anthropic_key
+BUILDER_LLM = Anthropic()
